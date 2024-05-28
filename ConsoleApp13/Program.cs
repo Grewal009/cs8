00ad45e5e88aa45b2a812904506ef66cf49a629d
Aneel Grewal <aneel.grewal@hotmail.com>
2024-05-28 18:54:44 +0200

    reverse of string using Reverse method

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter string: ");
+        string text = Console.ReadLine();
+        string reverseString = ReverseString(text);
+        Console.WriteLine($"Reverse of {text} is {reverseString}");
+    }
+
+    public static string ReverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
