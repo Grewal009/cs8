824798005435fa9cd7ef17fec34ec52016ed02ed
Aneel Grewal <aneel.grewal@hotmail.com>
2024-05-28 19:27:14 +0200

    replace character in a string

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -6,8 +6,26 @@ class Program
     {
         Console.Write("Enter string: ");
         string text = Console.ReadLine();
-        string reverseString = ReverseString(text);
-        Console.WriteLine($"Reverse of {text} is {reverseString}");
+
+        Console.WriteLine("press 1 to reverse string");
+        Console.WriteLine("press 2 to change character in a string ");
+
+        string input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "1":
+                string reverseString = ReverseString(text);
+                Console.WriteLine($"Reverse of {text} is {reverseString}");
+                break;
+            case "2":
+                ChangeCharacter(text);
+
+                break;
+            default:
+                Console.WriteLine("Exit");
+                break;
+        }
     }
 
     public static string ReverseString(string str)
@@ -15,5 +33,25 @@ class Program
         char[] charArray = str.ToCharArray();
         Array.Reverse(charArray);
         return new string(charArray);
+    }
+
+    public static void ChangeCharacter(string str)
+    {
+        Console.Write("Enter character you want to change and character replace with: ");
+        char preCh = Convert.ToChar(Console.Read());
+        char newCh = Convert.ToChar(Console.Read());
+        string newString = "";
+        foreach (char ch in str)
+        {
+            if (ch == preCh)
+            {
+                newString += newCh;
+            }
+            else
+            {
+                newString += ch;
+            }
+        }
+        Console.WriteLine($"\nold string: {str} => new string: {newString}");
     }
 }
